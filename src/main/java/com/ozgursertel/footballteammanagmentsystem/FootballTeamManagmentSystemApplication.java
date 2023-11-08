a9b1200ceed3edfede90e31eb652f00a7b220075
ozgursertel <sertel.ozgur@gmail.com>
2023-11-08 15:17:51 +0300

    Add Existing files to project

---
+++ src/main/java/com/ozgursertel/footballteammanagmentsystem/FootballTeamManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ozgursertel.footballteammanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTeamManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTeamManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozgursertel/footballteammanagmentsystem/FootballTeamManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozgursertel.footballteammanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTeamManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
